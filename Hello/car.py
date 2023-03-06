9ba5320fdb4425cc8a6f40f33eabc028f83d37ac
Carlos <carlosmateus7720@gmail.com>
2023-03-06 01:35:42 -0300

    oi oi oi

---
+++ Hello/car.py
@@ -0,0 +1,12 @@
+class Participante:
+    def __init__(self) -> None:
+        self.point = 0
+        self.choice = ""
+
+class GameRound:
+
+class Game:
+    def __init__(self) -> None:
+        self.endGame = False
+        self.participante = Participante()
+        self.secondParcipante = Participante()
